e985f206e3602581f1c172dfcf15d4dc51106c5f
黄春波 <yanlei3032@gmail.com>
2019-06-26 23:50:06 +0800

    m

--- src/list/LinkedList.go
+++ src/list/LinkedList.go
@@ -2,6 +2,7 @@ package list
 
 import (
 	"sync"
+	"fmt"
 )
 
 type Item int
@@ -13,10 +14,10 @@ type Node struct {
 }
 
 type LinkedList struct {
-	size int  //链表长度
-	last *Node
-	first *Node //跟节点
-	lock sync.RWMutex //读写锁对象
+	size  int //链表长度
+	last  *Node
+	first *Node        //跟节点
+	lock  sync.RWMutex //读写锁对象
 }
 
 func (list *LinkedList) Init() {
@@ -24,16 +25,16 @@ func (list *LinkedList) Init() {
 	(list).first = nil
 }
 
-func(list *LinkedList) addObj(t Item){
-	node :=&Node{nil,obj,nil}
+func (list *LinkedList) addObj(t Item) {
+	node := &Node{nil, t, nil}
 	(*list).add(node)
 }
 
-func (list *LinkedList) isEmpty() bool{
-	return (*list).size==0
+func (list *LinkedList) isEmpty() bool {
+	return (*list).size == 0
 }
 
-func (list *LinkedList) getSize() int  {
+func (list *LinkedList) getSize() int {
 	return (*list).size
 }
 
@@ -42,18 +43,31 @@ func (list *LinkedList) add(node *Node) {
 	defer list.lock.Unlock()
 	if (*list).size == 0 {
 		(*list).first = node //如果没有元素，跟节点赋值
-		(*list).last=node
-	}else{
-		(*list).last.next=node
-		node.pre=(*list).last
-		(*list).last=node;
+		(*list).last = node
+	} else {
+		(*list).last.next = node
+		node.pre = (*list).last
+		(*list).last = node;
 		(*list).size++
 	}
 }
 
-func(list *LinkedList) remove(node *Node) {
+func (list *LinkedList) RemoveAt(i int) (*Item, error) {
+	list.lock.Lock()
+	defer list.lock.Unlock()
 
+	if i < 0 || i > list.size {
+		return nil, fmt.Errorf("Index %d out of bonuds", i)
+	}
+
+	curNode := list.first
+	preIndex := 0
+	for preIndex < i-1 {
+		preIndex++
+		curNode = curNode.next
+	}
+	item := curNode.data
+	curNode.next = curNode.next.next
+	list.size--
+	return &item, nil
 }
-
-
-
