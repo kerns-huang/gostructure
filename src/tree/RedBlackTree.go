d3b8d20d6ce3893fd2b8e84cbc5f7b883f318601
黄春波 <yanlei3032@gmail.com>
2019-06-28 19:14:57 +0800

    添加红黑树的实现

--- src/tree/RedBlackTree.go
+++ src/tree/RedBlackTree.go
@@ -4,16 +4,14 @@ import "sync"
 
 type Item string
 
-type BRTreeNode struct {
+type RBTreeNode struct {
 	isRed bool
 	item Item
-	parent *BRTreeNode
-	left *BRTreeNode
-	right *BRTreeNode
+	parent,left,right *RBTreeNode
 }
 
 type BRTree struct {
-	root *BRTreeNode
+	root *RBTreeNode
 	size int
 	lock sync.RWMutex
 
@@ -28,22 +26,23 @@ func NewBRTree() *BRTree{
 func (tree *BRTree) Add(item Item){
 	//如果没有节点，就把新增的节点设置成跟节点
 	if  (*tree).root == nil {
-		(*tree).root = &BRTreeNode{true,item,nil,nil,nil}
+		(*tree).root = &RBTreeNode{true,item,nil,nil,nil}
 		tree.size=1
 		return
 	}
 }
 
 // 左旋操作,当前节点成为左节点
-func (tree *BRTree) rotateLeft(node *BRTreeNode){
-	 right := *node.right
-
-	 if (*node).parent == nil{ //如果节点的父亲是空的，说明node是根节点
-         tree.root = right
-	 }
+func (node *RBTreeNode) rotateLeft() (*RBTreeNode,error){
+	var root *RBTreeNode
+	 grandson :=node.right.left
+	 node.right.left=node
+	 node.parent=node.right
+	 node.right=grandson
+	 return root,nil
 
 }
 // 右旋操作，当前节点成为右节点
-func (tree *BRTree) rotateRight(node BRTreeNode){
+func (tree *BRTree) rotateRight(node RBTreeNode){
 
 }
