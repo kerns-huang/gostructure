f1f6c1496984dc2c159d176abf5b3ef1db67cc77
黄春波 <yanlei3032@gmail.com>
2019-07-11 13:53:21 +0800

    添加栈实现

--- src/list/Stack.go
+++ src/list/Stack.go
@@ -8,24 +8,33 @@ type Stack []interface {
 }
 
 func (stack Stack) Len() int {
+	// 计算数组的长度
 	return len(stack)
 }
 
 func (stack Stack) Empty() bool {
+	// 计算数组是否是空的
 	return len(stack) == 0
 }
 
-func (stack *Stack) push(value interface{}){
-	*stack=append(*stack,value)
+func (stack *Stack) push(value interface{}) {
+
+	*stack = append(*stack, value) //插入到最后一个数组
 }
 
-func (stack Stack) top() (interface{},error){
-	if stack.Empty(){
-		return nil,errors.New("out of index,len is 0")
+func (stack Stack) top() (interface{}, error) {
+	if stack.Empty() {
+		return nil, errors.New("out of index,len is 0")
 	}
-	return stack[len(stack)-1],nil
+	return stack[len(stack)-1], nil
 }
 
-func (stack Stack) pop() (interface{},error){
-
+func (stack *Stack) pop() (interface{}, error) {
+	theStack :=*stack
+	if stack.Empty() {
+		return nil, errors.New("stack is empty,len is 0")
+	}
+	item := theStack[len(theStack)-1]  //返回最后一个数组
+	*stack = theStack[0:len(theStack)-1] //重新生成切片
+	return item,nil
 }
---
+++ src/main/Base.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 1
+	var b *int = &a
+	var c **int = &b
+	var x int = *b
+	fmt.Println("a = ",a) // 取的是值对象
+	fmt.Println("&a = ",&a) //值的地址对象
+	fmt.Println("*&a = ",*&a) //指向地址的值对象
+	fmt.Println("b = ",b)
+	fmt.Println("&b = ",&b)
+	fmt.Println("*&b = ",*&b)
+	fmt.Println("*b = ",*b)
+	fmt.Println("c = ",c)
+	fmt.Println("*c = ",*c)
+	fmt.Println("&c = ",&c)
+	fmt.Println("*&c = ",*&c)
+	fmt.Println("**c = ",**c)
+	fmt.Println("***&*&*&*&c = ",***&*&*&*&*&c)
+	fmt.Println("x = ",x)
+}
+
